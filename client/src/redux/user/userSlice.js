a4932fd8b697eaba7729116b8dce23f227db6648
bimashazaman <bimashazaman1234@gmail.com>
2023-12-24 02:34:29 +0600

    Complete functional profile

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -3,10 +3,10 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
   currentUser: null,
   error: null,
-  loading: false, // It's better to initialize loading as false rather than null
+  loading: false,
 }
 
-export const userSlice = createSlice({
+const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
@@ -19,19 +19,61 @@ export const userSlice = createSlice({
       state.error = null
     },
     signInFailure: (state, action) => {
-      state.loading = false
       state.error = action.payload
+      state.loading = false
+    },
+    updateUserStart: (state) => {
+      state.loading = true
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload
+      state.loading = false
+      state.error = null
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload
+      state.loading = false
+    },
+    deleteUserStart: (state) => {
+      state.loading = true
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null
+      state.loading = false
+      state.error = null
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload
+      state.loading = false
+    },
+    signOutUserStart: (state) => {
+      state.loading = true
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null
+      state.loading = false
+      state.error = null
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload
+      state.loading = false
     },
   },
 })
 
-// Actions
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
+} = userSlice.actions
 
-// Selectors
-export const selectCurrentUser = (state) => state.user.currentUser
-export const selectUserLoading = (state) => state.user.loading
-export const selectUserError = (state) => state.user.error
-
-// Reducer
 export default userSlice.reducer
