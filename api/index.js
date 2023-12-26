2ac239add1f2ae8f0ba35fd136d1eff6b8e1bf5a
bimashazaman <bimashazaman1234@gmail.com>
2023-12-26 21:58:30 +0600

    Create listing API

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser'
 
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
 
 dotenv.config()
 
@@ -42,6 +43,7 @@ mongoose
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+app.use('/api/listing', listingRouter)
 
 // app.use(express.static(path.join(__dirname, '/client/dist')))
 
