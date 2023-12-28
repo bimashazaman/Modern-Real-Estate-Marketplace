95c654fce3ff9a1abc52a5849d29439aa08d2fdc
bimashazaman <bimashazaman1234@gmail.com>
2023-12-29 04:14:09 +0600

    Final

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,16 +7,26 @@ import 'swiper/css/bundle'
 import ListingItem from '../components/ListingItem'
 import { BASE_URL } from '../../constant'
 
+const getToken = () => localStorage.getItem('token')
+
 export default function Home() {
   const [offerListings, setOfferListings] = useState([])
   const [saleListings, setSaleListings] = useState([])
   const [rentListings, setRentListings] = useState([])
+  const token = getToken()
   SwiperCore.use([Navigation])
 
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?offer=true&limit=4')
+        const res = await fetch(
+          `${BASE_URL}/api/listing/get?offer=true&limit=4`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
         const data = await res.json()
         setOfferListings(data)
         fetchRentListings()
@@ -26,7 +36,14 @@ export default function Home() {
     }
     const fetchRentListings = async () => {
       try {
-        const res = await fetch(`${BASE_URL}/api/listing/get?type=rent&limit=4`)
+        const res = await fetch(
+          `${BASE_URL}/api/listing/get?type=rent&limit=4`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
         const data = await res.json()
         setRentListings(data)
         fetchSaleListings()
@@ -37,7 +54,14 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch(`${BASE_URL}/api/listing/get?type=sale&limit=4`)
+        const res = await fetch(
+          `${BASE_URL}/api/listing/get?type=sale&limit=4`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
         const data = await res.json()
         setSaleListings(data)
       } catch (error) {
@@ -46,6 +70,7 @@ export default function Home() {
     }
     fetchOfferListings()
   }, [])
+
   return (
     <div>
       {/* top */}
